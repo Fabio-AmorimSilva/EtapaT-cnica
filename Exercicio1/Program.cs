74e44afaca4afdb17ed449fe0aa664aa7c0f602b
Fabio <fabioamorim2025@hotmail.com>
2021-02-19 14:36:48 -0300

    Exercício 1 Completo

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int inicio;
+            int fim;
+
+            Console.WriteLine("Digite o multiplicando: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero < 0 || numero > 3000){
+                Console.WriteLine("O multiplicando não pode ser menor que zero ou maior que 3000.");
+                Console.WriteLine("Tente novamente.");
+                return;
+
+            }
+
+            Console.WriteLine("Digite o início do intervaldo de multiplicação: ");
+            inicio = int.Parse(Console.ReadLine());
+
+            if(inicio < 0 || inicio > 3000){
+                Console.WriteLine("O inicio não pode ser menor que zero ou maior que 3000.");
+                Console.WriteLine("Tente novamente.");
+                return;
+              
+
+            }
+
+            
+            Console.WriteLine("Digite o fim do intervaldo de mltiplicação: ");
+            fim = int.Parse(Console.ReadLine());
+
+            if(fim < 0 || fim > 3000){
+                Console.WriteLine("O fim não pode ser menor que zero ou maior que 3000.");
+                Console.WriteLine("Tente novamente.");
+                return;
+
+            }
+
+            if(inicio > fim){
+                Console.WriteLine("O inicio não pode ser maior que o final.");
+                Console.WriteLine("Tente novamente");
+                return;
+
+            }else if(fim - inicio >= 10){
+                Console.WriteLine("O intervalor de multiplicação não pode ser maior que  ou igual a 10.");
+                Console.WriteLine("Tente novamente");
+                return;
+            }
+
+
+            for(int i = inicio; i <= fim; i++){
+                Console.WriteLine(" " + numero + " x " + i + " = " + numero * i);
+
+
+            }
+
+            Console.WriteLine("Fim do intervalo");
+
+
+        }
+    }
+}
